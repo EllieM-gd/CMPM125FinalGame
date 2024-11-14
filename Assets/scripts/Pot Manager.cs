0c0b46b8e4ed8bff3c3a8b885edb362d12c41300
Hunter-Kingsley <hunterkingsley329@gmail.com>
2024-11-13 21:29:40 -0800

    Pot Pickup
    - Made front wall invisible
    - Added trigger volume for the player to interact with the pot
    - Added pot manager singleton to store reference to pot and other important pot-specific data
    - Added implementation for the player to be able to pick up the pot and place it back on the stove

---
+++ Assets/scripts/Pot Manager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotManager : MonoBehaviour
+{
+
+    private static PotManager _instance;
+
+    public static PotManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    [SerializeField] public GameObject Pot;
+    public bool IsPickedUp = false;
+    public Vector3 PotBasePosition = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PotBasePosition = Pot.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/PotPickupBehavior.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotPickupBehavior : MonoBehaviour
+{
+    private PotManager PotManager;
+    private PlayerManager PlayerManager;
+    private GameObject Pot;
+    private GameObject Player;
+    private Transform StoveTransform;
+    private bool PlayerInTrigger = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PotManager = PotManager.Instance;
+        Pot = PotManager.Pot;
+        PlayerManager = PlayerManager.Instance;
+        Player = PlayerManager.Player;
+        StoveTransform = this.gameObject.transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerInTrigger && Input.GetButtonDown("Fire2"))
+        {
+            if (PotManager.IsPickedUp)
+            {
+                Pot.transform.parent = StoveTransform;
+                Pot.transform.position = PotManager.PotBasePosition;
+                PotManager.IsPickedUp = false;
+            }
+            else
+            {
+                Pot.transform.parent = Player.transform;
+                Pot.transform.position = Player.transform.position + Player.transform.forward.normalized;
+                PotManager.IsPickedUp = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerInTrigger = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerInTrigger = false;
+        }
+    }
+}
