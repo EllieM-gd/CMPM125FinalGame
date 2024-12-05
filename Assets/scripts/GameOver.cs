abe19d3c8d79832e650ecd42223e108313486c06
Pallavi Rajeev <pallavirajeev44@gmail.com>
2024-12-04 20:01:42 -0800

    adding game over meal count

--- Assets/scripts/GameOver.cs
+++ Assets/scripts/GameOver.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOver : MonoBehaviour
 {
+    public TextMeshProUGUI mealCountText;  // Reference to the UI Text element
+
+    void Start()
+    {
+        // Retrieve the stored meal count from PlayerPrefs and display it
+        int mealCount = PlayerPrefs.GetInt("MealsServed", 0);  // Default to 0 if not found
+        mealCountText.text = "You Served " + mealCount + " Meals Correctly!";
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         SceneManager.LoadScene("Game Over");
